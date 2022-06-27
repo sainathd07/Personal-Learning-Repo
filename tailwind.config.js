7d0896d1417808bc883f741f5425cb1a8e69b6c0
Sainath <saisainath777@gmail.com>
2022-06-27 12:42:08 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+     spacing: {
+            'some key': {
+                1.5: '0.375rem'
+            }
+        },
+    },
+  },
+  plugins: [],
+}
